058f823a139db857774c4b15f64b325f93ed4e68
julionaponucena <julio.naponucena@hotmail.com>
2024-11-05 14:34:14 -0300

    Add vite-vercel-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
-export default{
-    root: 'src'
-}
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
+export default defineConfig({
+ root: 'src',
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [vercel()],
+});
